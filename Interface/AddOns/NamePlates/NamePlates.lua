c947d6db0d2820444729e365dbf51a650a930b12
AsuriTV <38655313+Tahiku@users.noreply.github.com>
2026-05-14 16:48:27 +0200

    Update NamePlates.lua
    Added Lightwell Coloring + name coloring
    Added spell name text fix
    Updated scaling on some NPCs

--- Interface/AddOns/NamePlates/NamePlates.lua
+++ Interface/AddOns/NamePlates/NamePlates.lua
@@ -17,10 +17,15 @@
 local activePlates = {}
 local max, strformat = math.max, string.format
 local ipairs, strfind = ipairs, string.find
+local max, min = math.max, math.min
 
 local RedName = {
     ["Viper"] = true,
     ["Venomous Snake"] = true,
+}
+
+local PinkName = {
+    ["Lightwell"] = true,
 }
 
 local ShrinkPlates = {
@@ -70,17 +75,17 @@ local ghoulPets = {
     "Casket", "Limb", "Worm", "Earth", "Spine", "Pebble", "Root", "Marrow", "Hammer"
 }
 
-local cIcon = { -- Edited / flipped textures are: Priest, Rogue, Paladin, Druid
-    ["MAGE"] = "Interface\\Icons\\inv_staff_13",
-    ["WARRIOR"] = "Interface\\Icons\\inv_sword_27",
-    ["HUNTER"] = "Interface\\Icons\\inv_weapon_bow_07",
-    ["WARLOCK"] = "Interface\\Icons\\spell_nature_drowsy",
-    ["SHAMAN"] = "Interface\\Icons\\spell_nature_bloodlust",
+local cIcon = { -- Edited / flipped textures are: Priest, Rogue, Paladin, Druid, Shaman
+    ["MAGE"]        = "Interface\\Icons\\inv_staff_13",
+    ["WARRIOR"]     = "Interface\\Icons\\inv_sword_27",
+    ["HUNTER"]      = "Interface\\Icons\\inv_weapon_bow_07",
+    ["WARLOCK"]     = "Interface\\Icons\\spell_nature_drowsy",
     ["DEATHKNIGHT"] = "Interface\\Icons\\spell_deathknight_classIcon",
-    ["DRUID"] = "Interface\\AddOns\\NamePlates\\medias\\classicon_druid.blp",
-    ["PALADIN"] = "Interface\\AddOns\\NamePlates\\medias\\inv_hammer_100.blp",
-    ["PRIEST"] = "Interface\\AddOns\\NamePlates\\medias\\inv_staff_300", -- inv_staff_30
-    ["ROGUE"] = "Interface\\AddOns\\NamePlates\\medias\\inv_throwingknife_0400", -- inv_throwingknife_04
+    ["DRUID"]       = "Interface\\AddOns\\NamePlates\\medias\\classicon_druid.blp",
+    ["PALADIN"]     = "Interface\\AddOns\\NamePlates\\medias\\inv_hammer_100.blp",
+    ["PRIEST"]      = "Interface\\AddOns\\NamePlates\\medias\\inv_staff_300", -- inv_staff_30
+    ["ROGUE"]       = "Interface\\AddOns\\NamePlates\\medias\\inv_throwingknife_0400", -- inv_throwingknife_04
+    ["SHAMAN"]      = "Interface\\AddOns\\NamePlates\\medias\\inv_jewelry_talisman_04", -- "Interface\\Icons\\spell_nature_bloodlust",
 }
 
 
@@ -89,10 +94,11 @@ local spellColors = {
     ["First Aid"] = { r = .97, g = .97, b = .97 },
     ["Hearthstone"] = { r = .97, g = .97, b = .97 },
     ["Attack"] = { r = .97, g = .97, b = .97 },
+    ["Volley"] = { r = .97, g = .97, b = .97 },
     ["Steady Shot"] = { r = .97, g = .97, b = .97 },
     ["Shattering Throw"] = { r = .97, g = .97, b = .97 },
     ["War Stomp"] = { r = .97, g = .97, b = .97 },
-
+    ["Basic Campfire"] = { r = .97, g = .97, b = .97 },
     ["Grand Spellstone"] = { r = .97, g = .97, b = .97 },
     ["Deadly Poison IX"] = { r = .97, g = .97, b = .97 },
     ["Wound Poison VII"] = { r = .97, g = .97, b = .97 },
@@ -111,6 +117,9 @@ local spellColors = {
     ["Explode"] = { r = .97, g = .97, b = .97 },
     ["Pin"] = { r = .97, g = .97, b = .97 },
     ["Huddle"] = { r = .97, g = .97, b = .97 },
+
+    -- PvE
+    -- ["Empower"] = { r = .97, g = .97, b = .97 }, -- Test mob in Dragonblight
 }
 
 local trackedUnits = {
@@ -124,7 +133,6 @@ local trackedUnits = {
 
 local unitKeys = {}
 local arenaNames = {}
-
 
 local function isGhoul(name, unit)
     local _, instanceType = IsInInstance()
@@ -167,9 +175,57 @@ local function getSpellColor(spellName)
 end
 
 
+------------------------------------------------------------------------------------------------------------------------------------------
+
+---- font scaling
+---- Add this somewhere near your utilities:
+-- local function UpdateFontScaling(plate)
+    -- local uf = plate.UnitFrame
+    -- if not uf then return end
+
+    -- local scale = plate:GetScale() or 1
+    -- if uf.lastScale and math.abs(uf.lastScale - scale) < 0.01 then -- Avoid constant updates (VERY IMPORTANT)
+        -- return
+    -- end
+
+    -- uf.lastScale = scale
+
+    ---- Base sizes (your defaults)
+    -- local baseNameSize = 13
+    -- local baseCastSize = 11
+    -- local baseHPSize   = 10
+
+    ---- Apply scaling (if you want to shrink the names)
+	---- local nameSize = max(9, min(16, baseNameSize * scale))
+	---- local castSize = max(7, baseCastSize * scale * 0.9)
+	---- local hpSize   = max(7, baseHPSize * scale)
+
+	---- Dampen scaling so fonts don't shrink as much
+	-- local fontScale = 0.7 + (scale * 0.3)
+	---- 70% fixed size, 30% follows plate scale
+
+	-- local nameSize = max(10, min(16, baseNameSize * fontScale))
+	-- local castSize = max(8, baseCastSize * fontScale)
+	-- local hpSize   = max(8, baseHPSize * fontScale)
+
+    ---- Apply fonts
+    -- if uf.newName then
+        -- uf.newName:SetFont("Interface\\AddOns\\NamePlates\\Prototype.ttf", nameSize, "OUTLINE") -- SetFont("Interface\\AddOns\\NamePlates\\Prototype.ttf", nameSize, "OUTLINE")
+    -- end
+    -- if plate.castText then
+        -- plate.castText:SetFont("Interface\\AddOns\\NamePlates\\Prototype.ttf", castSize, "OUTLINE") -- SetFont("Interface\\AddOns\\NamePlates\\Prototype.ttf", castSize, "OUTLINE")
+    -- end
+    -- if plate.timer then
+        -- plate.timer:SetFont("Interface\\AddOns\\NamePlates\\Prototype.ttf", castSize, "OUTLINE") -- SetFont("Interface\\AddOns\\NamePlates\\Prototype.ttf", castSize, "OUTLINE")
+    -- end
+    -- if uf.hptext then
+        -- uf.hptext:SetFont("Interface\\AddOns\\NamePlates\\Archangelsk.ttf", hpSize, "OUTLINE") -- SetFont("Interface\\AddOns\\NamePlates\\Prototype.ttf", hpSize, "OUTLINE")
+    -- end
+-- end
+-----------------------------------------------------------------------------------------------------------------------------------------
+
 local function PlayerClassIcons(nameplate, unit, nameText)
     local _, instanceType = IsInInstance()
-    
     if instanceType ~= "arena" then
         if nameplate.classTexture then
             nameplate.classTexture:Hide()
@@ -178,9 +234,8 @@ local function PlayerClassIcons(nameplate, unit, nameText)
     end
 
     local iconTexture = nil
-
     if nameText then
-        for i = 1, 3 do -- 5
+        for i = 1, 3 do -- Ignoring 5s, non-existant
             local arenaUnit = "arena" .. i
             if UnitName(arenaUnit) == nameText then
                 local _, unitClass = UnitClass(arenaUnit)
@@ -197,16 +252,10 @@ local function PlayerClassIcons(nameplate, unit, nameText)
 
     if not nameplate.classTexture then
         nameplate.classTexture = nameplate:CreateTexture(nil, "OVERLAY")
-
-        nameplate.classTexture:SetSize(22.6, 22.6) -- 22.4, 22.4
+        nameplate.classTexture:SetSize(22.6, 22.6)
         -- nameplate.classTexture:SetTexCoord(.06, .94, .06, .94)
-        nameplate.classTexture:SetPoint("RIGHT", nameplate, "RIGHT", 8, -7.4)
+        nameplate.classTexture:SetPoint("RIGHT", nameplate, "RIGHT", 8, -5.8) -- ("RIGHT", nameplate, "RIGHT", 8, -7.4)
         nameplate.classTexture:Hide()
-
-        -- nameplate.classTexture:SetSize(19, 19) -- 22.4, 22.4
-        ---- nameplate.classTexture:SetTexCoord(.1, .9, .1, .9)
-        -- nameplate.classTexture:SetPoint("RIGHT", nameplate, "RIGHT", 3, -7)
-        -- nameplate.classTexture:Hide()
     end
 
     if iconTexture then
@@ -219,8 +268,11 @@ end
 
 
 local function RenameOrColorPlates(nameplate, unit, HealthBar, name)
-    if name and RedName[name] and nameplate.newName then
+    if name and RedName[name] and nameplate.newName then -- Snakes
         nameplate.newName:SetTextColor(1, 0, 0) -- (0.65, 0.12, 0.15)
+	-- Lightwell
+    elseif name and PinkName[name] and nameplate.newName then
+        nameplate.newName:SetTextColor(.95, .75, .95) -- dark purple (0.45, 0.25, 0.95)
     elseif name and nameplate.newName then
         nameplate.newName:SetTextColor(1, 1, 1)
     end
@@ -262,6 +314,8 @@ local function RenameOrColorPlates(nameplate, unit, HealthBar, name)
         elseif strfind(name, "Fire Elemental") then displayName = "Ele"
         elseif name == "Water Elemental" then
             displayName = "wele"
+        elseif name == "Lightwell" then
+            HealthBar:SetStatusBarColor(0.95, 0.75, 0.95) -- 1.0, 0.4, 0.8
         elseif name == "Shadowfiend" then
             displayName = "Fiend"
         elseif name == "Spirit Wolf" then
@@ -316,6 +370,8 @@ local function PlateShowOrSize(nameplate, name, unit)
             end
         end
         nameplate:SetScale(scale)
+		-- UpdateFontScaling(nameplate) -- font scaling
+		-- nameplate.castBar:SetScale(scale) -- abc
     end
 end
 
@@ -344,8 +400,8 @@ local function InitNamePlate(plate)
     UnitFrame.castBarBorder = cbborder
     UnitFrame.cbicon = cbicon
     UnitFrame.cbshield = cbshield
-    cbicon:SetSize(16, 16) -- 16, 16
-    cbicon:SetTexCoord(.06, .94, .06, .94)
+    cbicon:SetSize(16, 15) -- 16, 16
+    cbicon:SetTexCoord(.1, .9, .1, .9)
     border:Hide()
     border:SetTexture(.3, .3, .3)
 
@@ -396,23 +452,24 @@ local function InitNamePlate(plate)
     -- spell text
     if not plate.castText then
         plate.castText = plate:CreateFontString(nil, "ARTWORK", "SystemFont_Outline") 
-        plate.castText:SetFont("Interface\\AddOns\\NamePlates\\Prototype.ttf", 12, "OUTLINE")
-        plate.castText:SetSize(120, 16)
-        plate.castText:SetPoint("CENTER", castBar, "CENTER", 0, 1)
+        plate.castText:SetFont("Interface\\AddOns\\NamePlates\\Prototype.ttf", 11, "OUTLINE") -- plate.castText:SetFont("Interface\\AddOns\\NamePlates\\Prototype.ttf", 11, "OUTLINE")
+        plate.castText:SetSize(150, 16) -- 120, 16
+        -- plate.castText:SetPoint("CENTER", castBar, "CENTER", -20, 0.5) -- plate.castText:SetPoint("CENTER", castBar, "CENTER", -20, 1)
+        -- plate.castText:SetJustifyH("RIGHT")
     end
 
     -- time
     if not plate.timer then
         plate.timer = plate:CreateFontString(nil, "ARTWORK", "SystemFont_Outline")
-        plate.timer:SetFont("Interface\\AddOns\\NamePlates\\Prototype.ttf", 12, "OUTLINE")
+        plate.timer:SetFont("Interface\\AddOns\\NamePlates\\Prototype.ttf", 11, "OUTLINE")
         plate.timer:SetSize(150, 16)
-        plate.timer:SetPoint("RIGHT", castBar, "RIGHT", 88, 0) -- ("RIGHT", castBar, "RIGHT", 92, 0)
+        plate.timer:SetPoint("RIGHT", castBar, "RIGHT", 86, 0) -- ("RIGHT", castBar, "RIGHT", 92, 0)
     end
 
     -- hp text
     if not UnitFrame.hptext then
         UnitFrame.hptext = newBorder:CreateFontString(nil, "OVERLAY", "SystemFont_Outline")
-        UnitFrame.hptext:SetFont("Interface\\AddOns\\NamePlates\\Prototype.ttf", 10, "OUTLINE")
+        UnitFrame.hptext:SetFont("Interface\\AddOns\\NamePlates\\Archangelsk.ttf", 10, "OUTLINE")
         UnitFrame.hptext:SetPoint("CENTER", healthBar, "CENTER", 2, 0)
         UnitFrame.hptext:SetJustifyH("CENTER")
         UnitFrame.hptext:SetTextColor(1, 1, 1)
@@ -422,10 +479,12 @@ local function InitNamePlate(plate)
     if not plate.barSpark then
         plate.barSpark = plate:CreateTexture(nil, "OVERLAY")
         plate.barSpark:SetTexture("Interface\\CastingBar\\UI-CastingBar-Spark")
-        plate.barSpark:SetSize(32, 32)
+        plate.barSpark:SetSize(14, 28) -- 32, 32
         plate.barSpark:SetPoint("CENTER", castBar, 0, 0)
         plate.barSpark:SetBlendMode("ADD")
     end
+
+	-- UpdateFontScaling(plate)
 end
 
 
@@ -542,7 +601,6 @@ scanFrame:SetScript("OnUpdate", function(self, elapsed)
     end
 
 
-
 ---------------------------------------------------------------------------------------------------
     for nameplate in pairs(activePlates) do
         if nameplate:IsShown() then
@@ -568,6 +626,8 @@ scanFrame:SetScript("OnUpdate", function(self, elapsed)
 					hptext:Hide()
 				end
 			end
+---------------------------------------------------------------------------------------------------
+
 
 ---------------------------------------------------------------------------------------------------
             if uf then
@@ -657,7 +717,7 @@ scanFrame:SetScript("OnUpdate", function(self, elapsed)
                             if maxValue > 0 then
                                 CastBarPosition(uf.castBar, "BOTTOMRIGHT", uf.healthBar, "BOTTOMRIGHT", 26, -16)
                                 CastBarPosition(uf.castBarBorder, "CENTER", nameplate, "CENTER", 35, -15.581398151124)
-                                CastBarPosition(uf.cbicon, "CENTER", uf.castBarBorder, "BOTTOMLEFT", -7, 8)
+                                CastBarPosition(uf.cbicon, "CENTER", uf.castBarBorder, "BOTTOMLEFT", -6, 8) -- -7, 8)
                                 CastBarPosition(uf.cbshield, "CENTER", uf, "CENTER", 13, -26.581398151124)
 
                                 uf.castBarBorder:Show()
@@ -666,10 +726,63 @@ scanFrame:SetScript("OnUpdate", function(self, elapsed)
                                 local r, g, b = getSpellColor(castName)
                                 cb:SetStatusBarColor(r, g, b)
 
-                                if nameplate.castText then
-                                    nameplate.castText:SetText(castName or "")
-                                    if not nameplate.castText:IsShown() then nameplate.castText:Show() end
-                                end
+								-- abc
+								-- if uf.cbshield:IsShown() then
+									-- cb:SetStatusBarColor(0.97, 0.97, 0.97)
+								-- end
+
+                                -- if nameplate.castText then
+                                    -- nameplate.castText:SetText(castName or "")
+                                    -- if not nameplate.castText:IsShown() then nameplate.castText:Show() end
+                                -- end
+
+------------------------------ abc ------------------------------------------------------------------------------------------------------------------
+								-- if nameplate.castText then
+									-- local text = castName or ""
+									-- nameplate.castText:SetText(text)
+									-- local textWidth = nameplate.castText:GetStringWidth() ---- Dynamically adjust position based on text width
+									-- if textWidth < 16 then -- abc
+										-- nameplate.castText:SetPoint("CENTER", cb, "CENTER", 0, 1)
+										-- nameplate.castText:SetJustifyH("CENTER")
+									-- else
+										-- nameplate.castText:SetPoint("LEFT", cb, "LEFT", -2, 1)
+										-- nameplate.castText:SetJustifyH("LEFT")
+									-- end
+
+									-- nameplate.castText:ClearAllPoints()
+									-- nameplate.castText:SetPoint("LEFT", cb, "LEFT", -2, 1) ---- Anchor from LEFT so it grows left naturally
+
+									-- Optional clamp: prevent overflow into icon
+									-- local maxWidth = cb:GetWidth() - 20
+									-- if textWidth > maxWidth then
+										-- nameplate.castText:SetWidth(maxWidth)
+										-- nameplate.castText:SetWordWrap(false)
+										-- nameplate.castText:SetJustifyH("LEFT")
+									-- else
+										-- nameplate.castText:SetWidth(0) ---- auto size
+									-- end
+									-- nameplate.castText:Show()
+									-- if not nameplate.castText:IsShown() then nameplate.castText:Show() end
+								-- end
+
+								-- opt 2
+								if nameplate.castText then
+									local text = castName or ""
+									nameplate.castText:SetText(text)
+									local textWidth = nameplate.castText:GetStringWidth()
+									-- nameplate.castText:SetText(text)
+
+									if textWidth < 16 then
+										nameplate.castText:SetPoint("CENTER", cb, "CENTER", 0, 0.5)
+										nameplate.castText:SetJustifyH("CENTER")
+									else
+										nameplate.castText:SetPoint("LEFT", cb, "LEFT", 2, 0.5)
+										nameplate.castText:SetJustifyH("LEFT")
+									end
+									if not nameplate.castText:IsShown() then nameplate.castText:Show() end
+								end
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
                                 if math.ceil(nameplate:GetAlpha()) == 1 and UnitExists("target") then
                                     TimerHook(nameplate, elapsed, value, maxValue, not isChanneling)
@@ -681,7 +794,7 @@ scanFrame:SetScript("OnUpdate", function(self, elapsed)
 
                                 if nameplate.barSpark then
                                     local sparkPosition = (value / maxValue) * cb:GetWidth()
-                                    nameplate.barSpark:SetPoint("CENTER", cb, "LEFT", sparkPosition, -1)
+                                    nameplate.barSpark:SetPoint("CENTER", cb, "LEFT", sparkPosition, -0.4)
                                     if not nameplate.barSpark:IsShown() then nameplate.barSpark:Show() end
                                 end
                             end
